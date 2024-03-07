e8224617c717bb0a7bc170a49e50fc6809372ef1
Tatev <34788695+desrapivonyak@users.noreply.github.com>
2024-03-07 21:27:29 +0400

    Problem 34

---
+++ problem_34.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+
+vector<int> searchRange(vector<int>& nums, int target) {
+	int left = 0;
+        int right = nums.size() - 1;
+        vector<int> ans {};
+        for(int i = 0; i < nums.size(); ++i) {
+            if(nums[i] == target) {
+                ans.push_back(i);
+            }
+            if(nums[i] > target) {
+                break;
+            }
+        }
+
+        if(ans.size() > 0) {
+            return {ans[0], ans[ans.size() - 1]};
+        }
+        return {-1, -1};
+}
